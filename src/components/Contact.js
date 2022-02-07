8027347c3e954c2b936aabe373cde047eda9fa53
Ifenna-Pascal <71331395+Ifenna-Pascal@users.noreply.github.com>
2022-02-07 21:32:59 +0100

    latest_commit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -21,18 +21,18 @@ function Contact() {
     e.preventDefault();
     setLoading(true)
     send(
-        process.env.SERVICE_ID,
-        process.env.TEMPLATE_ID1,
+        process.env.REACT_APP_SERVICE_ID,
+        process.env.REACT_APP_TEMPLATE_ID1,
         data,
-        process.env.USER_ID
+        process.env.REACT_APP_USER_ID
       )
         .then((response) => {
             if(response.status === 200) {
                 send(
-                  process.env.SERVICE_ID,
-                  process.env.TEMPLATE_ID2,
+                  process.env.REACT_APP_SERVICE_ID,
+                  process.env.REACT_APP_TEMPLATE_ID2,
                   data,
-                  'user_lkbG2582yvwX2KUsaPw0U'
+                  process.env.REACT_APP_USER_ID
                 )
             }else {
                 setError(true)
@@ -64,7 +64,7 @@ function Contact() {
                     </div>
                    <form onSubmit={onSubmit}>
                     { 
-                        console.log(process.env.USER_ID),
+                        console.log(process.env.REACT_APP_USER_ID),
                         error && (
                             <div id="alert-2" className="flex p-4 mb-4 bg-red-100 rounded-lg dark:bg-red-200" role="alert">
                                 <svg className="flex-shrink-0 w-5 h-5 text-red-700 dark:text-red-800" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
