fb7f440e548edb1f35b74d29345a36d24c5ed559
Ifenna-Pascal <71331395+Ifenna-Pascal@users.noreply.github.com>
2022-04-05 11:17:37 +0100

    finished

--- src/components/Contact_us.js
+++ src/components/Contact_us.js
@@ -16,24 +16,24 @@ function Phone_Content () {
                         </span>
                     </div>
                     <h1 className='lg:text-4xl text-3xl font-Inter text-center font-[800] mb-4  lg:px-3 lg:px-0 lg:mb-6 tracking-wider text-white leading-10'>The Path Forward </h1>
-                    <p className='text-base font-Poppins lg:px-0 lg:px-3 mb-4 lg:mb-6  text-left tracking-wide text-white'>There is a reason why cryptocurrencies are in the trillions of dollars. With the emergence of a global, decentralized ledger and computing system which continues to grow faster and more powerful, business is being disrupted. Trillions of dollars in transaction fees from traditional banking are being threatened by blockchain technology. Countless industries are at risk of being undercut by less-expensive, faster, and more secure solutions. Whether you are an established business looking to secure your company’s foothold or an upcoming organization looking to break into the market, we have the solution for you.</p>
+                    <p className='text-base font-Poppin pr-2 lg:px-3 mb-4 lg:mb-6  text-left tracking-wide text-white'>There is a reason why cryptocurrencies are in the trillions of dollars. With the emergence of a global, decentralized ledger and computing system which continues to grow faster and more powerful, business is being disrupted. Trillions of dollars in transaction fees from traditional banking are being threatened by blockchain technology. Countless industries are at risk of being undercut by less-expensive, faster, and more secure solutions. Whether you are an established business looking to secure your company’s foothold or an upcoming organization looking to break into the market, we have the solution for you.</p>
                     <button className='bg-[#BE173F]  lg:mx-0 lg:w-[40%] mb-6 text-white text-base lg:text-lg tracking-wider font-Inter lg:font-bold px-10 py-3 lg:py-4 rounded-lg' ><Link to="contact" smooth={true} duration={500} offset={-84} spy={true} exact="true"> Contact </Link></button>
                     <div className='w-[100%] flex-col lg:flex-row gap-y-5 justify-start lg:mt-8 items-start flex'>
                    <Fade left={true}>
                    <div className='flex items-center lg:mr-20'>
-                        <div className='flex bg-white items-center mr-4 p-2 w-12 h-12 rounded-lg'><AiOutlineMail className='w-12 h-12' /></div>
-                        <div>
+                        <div className='flex bg-white items-center mr-2 p-2 w-12 h-12 rounded-lg'><AiOutlineMail className='w-12 h-12' /></div>
+                        <div className=''>
                         <span className='font-Poppins text-base  text-white font-semibold' >Email</span><br></br>
-                        <span className='font-Poppins text-base tracking-widest text-white font-semibold' >inquiries@blockchainslc.com</span>
+                        <span className='font-Poppins text-sm tracking-widest text-white font-semibold' >inquiries@blockchainslc.com</span>
                         </div>
                     </div>
                    </Fade>
                        <Fade delay={500} right={true}>
                        <div className='flex items-center'>
-                        <div className='flex bg-white items-center p-2 w-12 h-12 mr-4 rounded-lg'><BsTelephone className='w-10 h-10'/></div>
+                        <div className='flex bg-white items-center p-2 w-12 h-12 mr-2 rounded-lg'><BsTelephone className='w-10 h-10'/></div>
                             <div>
                             <span className='font-Poppins text-base text-white block font-semibold'>Phone</span>
-                        <span className='font-Poppins text-base tracking-widest text-white font-semibold' >(801) 960-2447 </span>
+                        <span className='font-Poppins text-sm tracking-widest text-white font-semibold' >(801) 960-2447 </span>
                             </div>
                         </div>
                        </Fade>
